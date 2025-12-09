be9a0a9c7d32a3ef6df76725ebf1a88e547e44c9
rieleyhunt <huntrieley@gmail.com>
2025-12-08 22:30:09 -0500

    Implemented AJAX calls on restcountries API and entered callback hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -2,7 +2,7 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
+const country = "canada"
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -10,3 +10,88 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+const renderCountry = function(data) {
+
+    // Flag finder
+    const flags = data?.flags || {};
+    const flag_url =
+        flags?.svg ??
+        flags?.png ??
+        flags?.jpg ??
+        flags?.jpeg ??
+        Object.values(flags)[0];
+  
+    // Name finder
+    const names = data?.name || {};
+    const name =
+        names?.official ??
+        names?.common ??
+        Object.values(names)[0];
+
+    // Find continents
+    const continents = data?.continents ?? "No continent found";
+
+    // Find population
+    const population  = data?.population ?? "No population found";
+
+    // Find languages
+    const languages = data?.languages || {}
+    const language =
+        languages?.eng ??
+        languages?.fra ??
+        Object.values(languages)[0];
+
+    // Find currency
+    const currencies = data?.currencies || {}
+    const currency = Object.values(currencies)[0];
+    const currencyName =
+        currency?.name ??
+        Object.values(currency)[0];
+    
+    const html = `<article class="country">
+            <img class="country__img" src="${flag_url}" />
+            <div class="country__data">
+                <h3 class="country__name">${name}</h3>
+                <h4 class="country__region">${continents}</h4>
+                <p class="country__row"><span>👫</span>${population}</p>
+                <p class="country__row"><span>🗣️</span>${language}</p>
+                <p class="country__row"><span>💰</span>${currencyName}</p>
+            </div>
+            </article>`;
+    countriesContainer.insertAdjacentHTML('beforebegin', html);
+    countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbour = function (country) {
+
+    // AJAX call country 1
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+
+    request.addEventListener('load', function() {
+        const data = JSON.parse(this.responseText)[0];
+        console.log(data);
+        renderCountry(data);
+
+        // Get neigbour country
+    
+        for (let country of data.borders) {
+            const request = new XMLHttpRequest();
+            request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+            request.send();
+            request.addEventListener('load', function() {
+                const data = JSON.parse(this.responseText)[0];
+                renderCountry(data);
+            })
+        }
+     
+    });
+}
+
+btn.addEventListener('click', function () {    
+    getCountryAndNeighbour(country);
+    btn.style.display = "none";
+});
+
