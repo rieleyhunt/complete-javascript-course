9f2f7a41eff558d03269b32e7471ef1b15d3eb2b
Rieley <huntrieley@gmail.com>
2025-12-21 17:26:32 -0500

    Refactor lotteryPromise to include setTimeout for delay and improve error handling

--- 16-Asynchronous/starter/promise.js
+++ 16-Asynchronous/starter/promise.js
@@ -1,11 +1,28 @@
 
 const lotteryPromise = new Promise(function(resolve, reject) {
-    if (Math.random() >= 0.5) {
-        resolve('you WIN');
-    } else {
-        reject('you LOSE');
-    }
+    setTimeout(function() {
+        if (Math.random() >= 0.5) {
+            resolve('you WIN');
+        } else {
+            reject(new Error('you LOSE'));
+        }
+    }, 2000);
 });
 
 lotteryPromise.then(res => console.log(res)).catch
 (err => console.log(err));
+
+// Promisifying setTimeout
+const wait = function(seconds) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, seconds * 1000); // Calling resolve automatically resolves the Promise
+    });
+};
+
+wait(2).then(() => {
+    console.log("I waited for 2 seconds");
+    return wait(1);
+}).then(() => 
+    console.log("I waited for 1 second"));
+
+Promise.resolve('abc').then(x => console.log )
